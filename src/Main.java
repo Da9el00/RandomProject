b8144a67919f8255dbb61deff6bcd64a1dc96851
Daniel <Danielsmidstrup1@gmail.com>
2024-08-15 08:30:10 +0200

    More sout

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         System.out.println("Hello test!!!!!!");
 
         System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
+        System.out.println("test5");
+        System.out.println("test6");
+        System.out.println("test7");
     }
 }
